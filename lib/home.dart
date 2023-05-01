65bd2aa5eb548d4a5363babb71a386333156abf3
jeriQ24 <jerichocleo.quilas@wvsu.edu.ph>
2023-05-01 16:42:43 +0800

    added home

---
+++ lib/home.dart
@@ -0,0 +1,40 @@
+
+import 'package:exercise_228/component/my_drawer.dart';
+import 'package:flutter/material.dart';
+import 'component/collections.dart';
+import 'component/searchfield.dart';
+
+class Home extends StatelessWidget {
+
+  const Home({super.key,
+  
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+    appBar: AppBar(
+    backgroundColor: Colors.orangeAccent,
+    title: const Center(
+      child: Text("Manny's Hardware",
+      ),
+    ),
+    ),
+
+    drawer: const MyDrawer(),
+    body: const Center(
+      child: Column(
+        children: [
+         SizedBox(height: 25),
+        //a text field to handle searching
+        SearchField(),
+        SizedBox(height: 25),
+
+        //list of items to display collection
+        Collections(),
+        ],
+      ),
+      ),
+    );
+  }
+}
